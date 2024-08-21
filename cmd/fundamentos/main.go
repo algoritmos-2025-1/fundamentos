f3abc143b12bfb43bdf8a7eac6f5d726e9548409
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2024-08-21 09:50:59 -0600

    Main file changes for testing up to graph search algorithm

--- cmd/fundamentos/main.go
+++ cmd/fundamentos/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"fmt"
 
+	"github.com/algoritmos-2025-1/fundamentos/internal/collection"
 	"github.com/algoritmos-2025-1/fundamentos/internal/queue"
+	"github.com/algoritmos-2025-1/fundamentos/pkg/algorithms"
+	"github.com/algoritmos-2025-1/fundamentos/pkg/graph"
 )
 
 func main() {
 	q := queue.CreateSliceQueue[int]()
 	q.Enqueue(42)
 	fmt.Println(q.Dequeue())
+
+	c := collection.Collection[int](q)
+	println(c)
+
+	g := graph.CreateAdjacencyListGraph(12).(*graph.AdjacencyListGraph)
+	g.AddEdge(0, 11)
+	g.AddEdge(2, 11)
+	g.AddEdge(3, 11)
+	g.AddEdge(1, 10)
+	g.AddEdge(0, 1)
+	g.Print()
+
+	println(g.Order())
+	println(g.Size())
+
+	p, t := algorithms.GraphSearch(g, 0, c)
+	fmt.Println(p)
+	fmt.Println(t)
 }
